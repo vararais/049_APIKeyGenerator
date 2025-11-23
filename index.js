0c0fac0a60049c4c27436c2c53391588f58c4968
vararais <varadigmaisnann@gmail.com>
2025-11-23 23:55:48 +0700

    Menambah connection ke db dan prefix serta secret

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static("public"));
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+const KEY_PREFIX = "An4n_R4is_";
+const JWT_SECRET = process.env.JWT_SECRET;
